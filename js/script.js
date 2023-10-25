e07676c056efa7492ece82dd69ec75178dd22fe7
osj2d <juniorsavi97@gmail.com>
2023-10-25 20:42:20 -0300

    iniciando

---
+++ js/script.js
@@ -0,0 +1,7 @@
+echo "# keyvalues" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/osj2d/keyvalues.git
+git push -u origin main
