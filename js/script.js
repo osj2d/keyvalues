af6b9c4dc9fe53bfd9ae78467b0628222e2256dc
osj2d <juniorsavi97@gmail.com>
2023-10-26 19:54:18 -0300

    finalizando

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
 const tela = document.body;
 
-tela.addEventListener('keydown', (e) => {
-    const textArea = document.querySelector('.caixa')
-    let valores = e;
-    console.log(valores)
-    textArea.value += `Value: ${valores.key} - Code: ${valores.code} - Alt key: ${valores.altKey} - Control key: ${valores.ctrlKey} - Meta key: ${valores.metaKey} - Shift key: ${valores.shiftKey} \n` ;
-})
+function corDeFundo(n) {
+  let valor = n.length;
+  if (valor === 1) {
+    return `#${n}${n}${n}`;
+  } else if (valor === 2) {
+    return `#${n}${n}${n}`;
+  } else if (valor === 3) {
+    return `#${n}${n}`;
+  } else {
+    return "#fff";
+  }
+}
+
+tela.addEventListener("keydown", (e) => {
+  const textArea = document.querySelector(".caixa");
+  let valores = e;
+  textArea.value += `Value: ${valores.key} - Code: ${valores.code} - Alt key: ${valores.altKey} - Control key: ${valores.ctrlKey} - Meta key: ${valores.metaKey} - Shift key: ${valores.shiftKey} \n`;
+  const cor = corDeFundo(valores.keyCode.toString());
+  tela.style.background = `${cor}`;
+});
